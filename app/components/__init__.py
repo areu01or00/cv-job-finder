d27d84e6768021caaf28300c78937b3e15ce0aec
IndianStreetBets <shokatalishaikh95@gmail.com>
2025-03-17 20:51:37 +0530

    Add components package init

---
+++ app/components/__init__.py
@@ -0,0 +1,6 @@
+"""Components for the CV-Based Job Finder application."""
+
+from .cv_analyzer import CVAnalyzerComponent
+from .job_search import JobSearchComponent
+
+__all__ = ["CVAnalyzerComponent", "JobSearchComponent"]
